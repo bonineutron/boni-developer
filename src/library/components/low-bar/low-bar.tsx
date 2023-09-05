dfc36822be169c3e5530ac823c0982a69d9b66b0
Bonilla Andres <mailofbonilla@gmail.com>
2023-09-05 15:30:25 -0500

    feat: :sparkles: add contact modal

--- src/library/components/low-bar/low-bar.tsx
+++ src/library/components/low-bar/low-bar.tsx
@@ -1,4 +1,4 @@
-import { LanguageModal } from '../modal/all-modals';
+import { ContactModal, LanguageModal } from '../modal/all-modals';
 import { useTranslation } from 'react-i18next';
 import { HiLanguage } from 'react-icons/hi2';
 import { useState } from 'react';
@@ -9,6 +9,7 @@ export function LowBar(): JSX.Element {
 
   // states
   const [showLanguageModal, setShowLanguageModal] = useState<boolean>(false);
+  const [showContactModal, setShowContactModal] = useState<boolean>(false);
 
   return (
     <div className='absolute bottom-0 h-[80px] w-full'>
@@ -19,9 +20,12 @@ export function LowBar(): JSX.Element {
           <HiLanguage className='text-[22px]' />
           <span>{t('lowBar.language')}</span>
         </button>
-        <button className='custom-button'>{t('lowBar.contact')}</button>
+        <button onClick={() => setShowContactModal(true)} className='custom-button'>
+          {t('lowBar.contact')}
+        </button>
       </div>
       {showLanguageModal && <LanguageModal closeClick={setShowLanguageModal} />}
+      {showContactModal && <ContactModal closeClick={setShowContactModal} />}
     </div>
   );
 }
--- src/library/components/modal/all-modals/contact-modal/contact.modal.tsx
+++ src/library/components/modal/all-modals/contact-modal/contact.modal.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { IoLogoWhatsapp } from 'react-icons/io';
+import { RiMailSendFill } from 'react-icons/ri';
+import { Modal } from '../../modal';
 
-export function ContactModal(): JSX.Element {
-  return <div>contact-modal</div>;
+interface ContactModalProps {
+  closeClick: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function ContactModal({ closeClick }: ContactModalProps): JSX.Element {
+  // styles
+  const optionContact: string = 'custom-button flex items-center gap-2 w-[200px] bg-white text-black';
+  const iconContact: string = 'text-[30px]';
+
+  return (
+    <Modal closeClick={closeClick}>
+      <div className='flex flex-col gap-[20px]'>
+        <a
+          href='https://www.linkedin.com/in/bonineutron/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className={optionContact}>
+          <FaLinkedin className={iconContact} />
+          <span>Linkedin</span>
+        </a>
+        <a href='https://github.com/bonineutron' target='_blank' rel='noopener noreferrer' className={optionContact}>
+          <FaGithub className={iconContact} />
+          <span>Github</span>
+        </a>
+        <a href='mailto:mailofbonilla@gmail.com' target='_blank' rel='noopener noreferrer' className={optionContact}>
+          <RiMailSendFill className={iconContact} />
+          <span>Email</span>
+        </a>
+        <a
+          href='https://api.whatsapp.com/send?phone=573203604288'
+          target='_blank'
+          rel='noopener noreferrer'
+          className={optionContact}>
+          <IoLogoWhatsapp className={iconContact} />
+          <span>Whatsapp</span>
+        </a>
+      </div>
+    </Modal>
+  );
 }
