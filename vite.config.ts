562dc851deed83255271054dac5802b3cb5b6e29
Andres F. Bonilla Q. <mailofbonilla@gmail.com>
2024-01-04 01:47:18 -0500

    fix: :bug: resolve bug vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    legacy({
+      modernPolyfills: true,
+      renderLegacyChunks: true
+    })
+  ]
 });
